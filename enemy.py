7acca92df6262d86b537f70933537a4dc586fed8
Lee <lees_taylor_18@yahoo.com>
2021-08-31 20:11:29 +0100

    Removed bug preventing main.py from running

--- enemy.py
+++ enemy.py
@@ -3,9 +3,9 @@ import pygame
 import random
 import time
 from typing import NoReturn, List, Tuple
-from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.core.grid              import Grid
-from pathfinding.finder.a_star          import AStarFinder
+# from pathfinding.core.diagonal_movement import DiagonalMovement
+# from pathfinding.core.grid              import Grid
+# from pathfinding.finder.a_star          import AStarFinder
 
 
 p      = 'assets/'
@@ -41,8 +41,8 @@ class Enemy:
                          '' : self.null}
         self.player = None  # set when near player
         self.level  = lvl
-        self.grid   = Grid(matrix=self.level)
-        self.finder = AStarFinder(diagonal_movement=0)
+        # self.grid   = Grid(matrix=self.level)
+        # self.finder = AStarFinder(diagonal_movement=0)
         self.clock  = time.time()
         self.path   = []
         self.idle   = False
@@ -94,44 +94,46 @@ class Enemy:
         if (not tiles.get(self.face)): self.face = ''
 
     def movement_behaviour(self) -> NoReturn:
-        # If there is 'no' player in sight: ...
-        if (self.player == None):  
-            if(time.time() > self.clock + (self.interval * 2)):
-                self.set_direction(self.faces.get(random.randint(0,3)))
-                self.clock = time.time()
-                self.check_tile()
-                self.steps.get(self.face)()
-        # If there 'is a' player in sight: ...
-        elif (self.player != None):
-            if (self.path == []):  # Calculate + init. path
-                self.path = self.pathfind()
-                if (len(self.path) > 0): del self.path[0]
-            if (time.time() > self.clock + self.interval) and (len(self.path) > 0):  # Step for each interval
-                self.clock = time.time()
-                if   (self.ptr[1] - self.path[0][0] == 1) : self.set_direction('w')
-                elif (self.ptr[1] - self.path[0][0] == -1): self.set_direction('e')
-                elif (self.ptr[0] - self.path[0][1] == 1) : self.set_direction('n')
-                elif (self.ptr[0] - self.path[0][1] == -1): self.set_direction('s')
-                self.check_tile()
-                self.steps.get(self.face)()
-        # handle x-axis movement | x = matrix_ptr_x, vx = visual_x_coordinate
-        if ((x:=self.ptr[1]) != (vx:=int((self.xos + self.x)) / 80)): 
-            if   (x > vx): self.xos += 4
-            elif (x < vx): self.xos -= 4
-        # handle y-axis movement | y = matrix_ptr_y, vy = visual_y_coordinate
-        if ((y:=self.ptr[0]) != (vy:=int((self.yos + self.y)) / 80)):
-            if   (y > vy): self.yos += 4
-            elif (y < vy): self.yos -= 4
+        # # If there is 'no' player in sight: ...
+        # if (self.player == None):
+        #     if(time.time() > self.clock + (self.interval * 2)):
+        #         self.set_direction(self.faces.get(random.randint(0,3)))
+        #         self.clock = time.time()
+        #         self.check_tile()
+        #         self.steps.get(self.face)()
+        # # If there 'is a' player in sight: ...
+        # elif (self.player != None):
+        #     if (self.path == []):  # Calculate + init. path
+        #         self.path = self.pathfind()
+        #         if (len(self.path) > 0): del self.path[0]
+        #     if (time.time() > self.clock + self.interval) and (len(self.path) > 0):  # Step for each interval
+        #         self.clock = time.time()
+        #         if   (self.ptr[1] - self.path[0][0] == 1) : self.set_direction('w')
+        #         elif (self.ptr[1] - self.path[0][0] == -1): self.set_direction('e')
+        #         elif (self.ptr[0] - self.path[0][1] == 1) : self.set_direction('n')
+        #         elif (self.ptr[0] - self.path[0][1] == -1): self.set_direction('s')
+        #         self.check_tile()
+        #         self.steps.get(self.face)()
+        # # handle x-axis movement | x = matrix_ptr_x, vx = visual_x_coordinate
+        # if ((x:=self.ptr[1]) != (vx:=int((self.xos + self.x)) / 80)):
+        #     if   (x > vx): self.xos += 4
+        #     elif (x < vx): self.xos -= 4
+        # # handle y-axis movement | y = matrix_ptr_y, vy = visual_y_coordinate
+        # if ((y:=self.ptr[0]) != (vy:=int((self.yos + self.y)) / 80)):
+        #     if   (y > vy): self.yos += 4
+        #     elif (y < vy): self.yos -= 4
+        pass
 
-    def pathfind(self) -> List[Tuple[int, int]]:
-        '''
-            Returns list of coordinates from enemy to player
-        '''
-        self.grid.cleanup()  # Resets <Grid Object> for recalculation (necessary)
-        start     = self.grid.node(self.ptr[1], self.ptr[0])
-        end       = self.grid.node(self.player.x, self.player.y)
-        path, run = self.finder.find_path(start, end, self.grid)
-        return path
+    def pathfind(self): # -> List[Tuple[int, int]]:
+        # '''
+        #     Returns list of coordinates from enemy to player
+        # '''
+        # self.grid.cleanup()  # Resets <Grid Object> for recalculation (necessary)
+        # start     = self.grid.node(self.ptr[1], self.ptr[0])
+        # end       = self.grid.node(self.player.x, self.player.y)
+        # path, run = self.finder.find_path(start, end, self.grid)
+        # return path
+        return None
 
     """ Health methods and main draw (render) method """
 
