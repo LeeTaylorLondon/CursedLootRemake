bb2dc875ff4f1a7221441d09ab60ef99ec40ca5a
Lee <lees_taylor_18@yahoo.com>
2021-09-04 21:27:02 +0100

    Developing A* plotting mags positive x is working

--- experimental/pathfinding/a_star.py
+++ experimental/pathfinding/a_star.py
@@ -21,19 +21,31 @@ def plotmags(matrix:List[List[int]], start_coords=None):
             for x,n in enumerate(vec):
                 if n == 0:
                     start_coords = (x,y)
+    for y,vec in enumerate(matrix):
+        for x,n in enumerate(vec):
+            if matrix[y][x] == 0: continue
+            matrix[y][x] = None
     queue = [start_coords]
+    d_counter = 0
     while len(queue) > 0:
+        d_counter += 1
         x,y = queue.pop()
-        print(x, y)
+        print(f"y:{y},x:{x}")
         mag = rv[y][x]
-        try:
-            if not y-1 < 0:
-                rv[y-1][x] = mag + 1
+        if not y-1 < 0:
+            if rv[y-1][x] is None:
                 queue.append((x, y-1))
-            print2d(rv)
-        except IndexError:
-            pass
-    print2d(rv)
+            rv[y-1][x] = mag + 1
+        if not x+1 >= len(rv[y]):
+            if rv[y][x+1] is None:
+                queue.append((x+1, y))
+            rv[y][x+1] = mag + 1
+        # if not x-1 < 0:
+        #     rv[y][x-1] = mag + 1
+        #     queue.append((x-1, y))
+        # # print2d(rv)
 
+    print2d(rv)
+    print(d_counter)
 
 plotmags(grid)
