b847c9fea3077638aabfbb1e558de70f07bf8d49
Lee <lees_taylor_18@yahoo.com>
2021-09-05 21:16:03 +0100

    Found working code for A*

--- experimental/pathfinding/a_star.py
+++ experimental/pathfinding/a_star.py
@@ -17,6 +17,8 @@ def plotmags(matrix:List[List[int]], start_coords=None):
         for x,n in enumerate(vec):
             if matrix[y][x] == 0: continue
             matrix[y][x] = None
+
+    # up, right, left = [^ > <]
     queue = [start_coords]
     d_counter = 0
     while len(queue) > 0:
@@ -28,14 +30,17 @@ def plotmags(matrix:List[List[int]], start_coords=None):
             if rv[y][x-1] is None:
                 queue.append((x-1, y))
                 rv[y][x-1] = mag + 1
+
         if not x+1 >= len(rv[y]): # right
             if rv[y][x+1] is None:
                 queue.append((x+1, y))
                 rv[y][x+1] = mag + 1
+
         if not y-1 < 0: # up
             if rv[y-1][x] is None:
                 queue.append((x, y-1))
                 rv[y-1][x] = mag + 1
+
         print2d(rv)
 
     queue = [start_coords]
@@ -48,6 +53,10 @@ def plotmags(matrix:List[List[int]], start_coords=None):
             if rv[y][x-1] is None:
                 queue.append((x-1, y))
                 rv[y][x-1] = mag + 1
+        # if not y-1 < 0: # up
+        #     if rv[y-1][x] is None:
+        #         queue.append((x, y-1))
+        #         rv[y-1][x] = mag + 1
         if not x+1 >= len(rv[y]): # right
             if rv[y][x+1] is None:
                 queue.append((x+1, y))
@@ -60,12 +69,21 @@ def plotmags(matrix:List[List[int]], start_coords=None):
     print2d(rv)
     print(f"Actions: {d_counter}")
 
+
+def astar():
+    pass
+
+
 grid = [[1 for x in range(20)] for arr in range(20)]
 
-starting_coords = (6, 2)
+starting_coords = (2, 6)
+x0, y0 = starting_coords
 
-grid[6][2] = 0
+for y in range(3, 10):
+    grid[y][11] = 0
 
+grid[y0][x0] = 0
 print2d(grid)
 
-plotmags(grid, (2,6))
+plotmags(grid, (x0, y0))
+
